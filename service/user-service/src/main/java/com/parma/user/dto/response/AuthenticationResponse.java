e83bcfd3f04b5e3bb9bc953e1f43304140fb1dd2
petsereypanha <sereypanhapet@gmail.com>
2025-08-07 23:52:08 +0700

    add more refresh token service

---
+++ service/user-service/src/main/java/com/parma/user/dto/response/AuthenticationResponse.java
@@ -0,0 +1,8 @@
+package com.parma.user.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record AuthenticationResponse(
+        @JsonProperty("access_token") String accessToken,
+        @JsonProperty("refresh_token") String refreshToken){
+}
--- service/user-service/src/main/java/com/parma/user/service/impl/RefreshTokenService.java
+++ service/user-service/src/main/java/com/parma/user/service/impl/RefreshTokenService.java
@@ -3,6 +3,10 @@ package com.parma.user.service.impl;
 import com.parma.common.constant.ApiConstant;
 import com.parma.common.dto.EmptyObject;
 import com.parma.common.exception.CustomMessageException;
+import com.parma.common.exception.ResponseErrorTemplate;
+import com.parma.user.dto.request.RefreshTokenRequest;
+import com.parma.user.dto.response.AuthenticationResponse;
+import com.parma.user.model.CustomUserDetail;
 import com.parma.user.model.RefreshToken;
 import com.parma.user.model.User;
 import com.parma.user.repository.RefreshTokenRepository;
@@ -54,7 +58,52 @@ public class RefreshTokenService {
         log.info("Refresh refreshToken created successfully..!");
         refreshTokenRepository.save(refreshToken);
     }
+    public ResponseErrorTemplate refreshToken(RefreshTokenRequest refreshTokenRequest) {
+        try {
+            ResponseErrorTemplate errorTemplate = this.findByToken(refreshTokenRequest.refreshToken())
+                    .map(this::verifyExpiration)
+                    .map(RefreshToken::getUser)
+                    .map(userInfo -> {
+                        final CustomUserDetail customUserDetail = customUserDetailService.customUserDetail(userInfo.getUsername());
+                        final String accessToken = "SSS";//jwtService.generateToken(customUserDetail);
+                        var responseToken = new AuthenticationResponse(accessToken, refreshTokenRequest.refreshToken());
+                        return new ResponseErrorTemplate(
+                                ApiConstant.REFRESH_TOKEN_SUCCESS.getDescription(),
+                                ApiConstant.REFRESH_TOKEN_SUCCESS.getKey(),
+                                responseToken,
+                                false);
+                    }).orElseThrow();
+            return errorTemplate;
+        }catch (Exception e) {
+            log.error("Error while refreshing token with request: {}", refreshTokenRequest, e);
+            throw new CustomMessageException(
+                    ApiConstant.UN_AUTHORIZATION.getDescription(),
+                    ApiConstant.UN_AUTHORIZATION.getKey(),
+                    new EmptyObject(),
+                    HttpStatus.UNAUTHORIZED
+            );
+        }
+    }
 
+    public Optional<RefreshToken> findByToken(String token) {
+        return refreshTokenRepository.findByToken(token);
+    }
 
+    public RefreshToken verifyExpiration(RefreshToken token) {
+        if (token.getExpiryDate().compareTo(Date.from(Instant.now())) < 0) {
+            refreshTokenRepository.delete(token);
+            log.info("Refresh refreshToken is expired. Please make a new login..!");
+            throw new SystemException(token.getToken() + " Refresh refreshToken is expired. Please make a new login..!");
+        }
+        return token;
+    }
+
+    public void deleteToken(String token) {
+        refreshTokenRepository.deleteByToken(token);
+    }
+
+    public void deleteAllTokenByUserId(List<User> users) {
+        refreshTokenRepository.deleteAllByUserIn(users);
+    }
 
 }
